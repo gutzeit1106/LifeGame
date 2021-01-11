c8b77c86f2d1c1086af3aa6844323465d853914b
y10e <y10exxx@gmail.com>
2021-01-11 22:42:58 +0900

    tcell sample

---
+++ go/go.mod
@@ -0,0 +1,10 @@
+module go
+
+go 1.13
+
+require (
+	github.com/gdamore/tcell v1.4.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	golang.org/x/sys v0.0.0-20210110051926-789bb1bd4061 // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
--- go/main.go
+++ go/main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"github.com/gdamore/tcell"
+)
 
 func main() {
-	fmt.Println("Hello, World!");
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = screen.Init(); err != nil {
+		log.Fatal(err)
+	}
+	defer screen.Fini()
 
+	for i := 0; i < 10; i++ {
+		screen.SetContent(i, i, 'a', nil, tcell.StyleDefault)
+	}
+	screen.Show()
+
+	quit := make(chan struct{})
+	go func() {
+		for {
+			ev := screen.PollEvent()
+			switch ev.(type) {
+			case *tcell.EventKey:
+				close(quit)
+			}
+		}
+	}()
+	<-quit
 }
